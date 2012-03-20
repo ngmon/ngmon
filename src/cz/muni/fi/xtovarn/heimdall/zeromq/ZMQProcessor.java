a03017683b57edd348b8d76524ebf8ec4f942638
Dan Tovarnak <dan.tovarnak@gmail.com>
2012-03-20 14:53:36 +0000

    added new classes

--- src/cz/muni/fi/xtovarn/heimdall/zeromq/ZMQProcessor.java
+++ src/cz/muni/fi/xtovarn/heimdall/zeromq/ZMQProcessor.java
@@ -7,62 +7,62 @@ import org.zeromq.ZMQException;
 
 /**
  * ZeroMQ Forwarder Device implementation.
- * 
+ *
  * @author Alois Belaska <alois.belaska@gmail.com>
  */
 public class ZMQProcessor implements Runnable {
 
-    private final ZMQ.Poller poller;
-    private final ZMQ.Socket inSocket;
-    private final ZMQ.Socket outSocket;
+	private final ZMQ.Poller poller;
+	private final ZMQ.Socket inSocket;
+	private final ZMQ.Socket outSocket;
+	private final MessageProcessor processor;
 
-    /**
-     * Class constructor.
-     * 
-     * @param context
-     *            a 0MQ context previously created.
-     * @param inSocket
-     *            input socket
-     * @param outSocket
-     *            output socket
-     */
-    public ZMQProcessor(Context context, Socket inSocket, Socket outSocket) {
-        this.inSocket = inSocket;
-        this.outSocket = outSocket;
+	/**
+	 * Class constructor.
+	 *
+	 * @param context	a 0MQ context previously created.
+	 * @param inSocket  input socket
+	 * @param outSocket output socket
+	 */
+	public ZMQProcessor(Context context, Socket inSocket, Socket outSocket, MessageProcessor processor) {
+		this.inSocket = inSocket;
+		this.outSocket = outSocket;
 
-        this.poller = context.poller(1);
-        this.poller.register(inSocket, ZMQ.Poller.POLLIN);
-    }
+		this.poller = context.poller(1);
+		this.poller.register(inSocket, ZMQ.Poller.POLLIN);
 
-    /**
-     * Forwarding messages.
-     */
-    @Override
+		this.processor = processor;
+	}
+
+	/**
+	 * Forwarding messages.
+	 */
+	@Override
 	public void run() {
-        byte[] msg = null;
-        boolean more = true;
+		byte[] msg = null;
+		boolean more = true;
 
-        while (!Thread.currentThread().isInterrupted()) {
-            try {
-                // wait while there are requests to process
-                if (poller.poll(250000) < 1) {
-                    continue;
-                }
+		while (!Thread.currentThread().isInterrupted()) {
+			try {
+				// wait while there are requests to process
+				if (poller.poll(250000) < 1) {
+					continue;
+				}
 
-                msg = inSocket.recv(0);
+				msg = inSocket.recv(0);
+				more = inSocket.hasReceiveMore();
 
-                more = inSocket.hasReceiveMore();
+				if (msg != null) {
 
-                if (msg != null) {
-                    outSocket.send(msg, more ? ZMQ.SNDMORE : 0);
-                }
-            } catch (ZMQException e) {
-                // context destroyed, exit
-                if (ZMQ.Error.ETERM.getCode() == e.getErrorCode()) {
-                    break;
-                }
-                throw e;
-            }
-        }
-    }
+					outSocket.send(processor.process(msg)[0], more ? ZMQ.SNDMORE : 0);
+				}
+			} catch (ZMQException e) {
+				// context destroyed, exit
+				if (ZMQ.Error.ETERM.getCode() == e.getErrorCode()) {
+					break;
+				}
+				throw e;
+			}
+		}
+	}
 }
