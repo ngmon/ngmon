023fa5cb390fb2dd3d4d1421c5f6e68250985ec5
Dan Tovarnak <dan.tovarnak@gmail.com>
2012-03-26 07:27:58 +0000

    cleanup

--- src/cz/muni/fi/xtovarn/heimdall/zeromq/deprecated/ZMQMessageProcessor.java
+++
@@ -1,9 +0,0 @@
-package cz.muni.fi.xtovarn.heimdall.zeromq.deprecated;
-
-import org.zeromq.ZMQException;
-
-import java.util.List;
-
-public interface ZMQMessageProcessor {
-	public List<byte[]> process(List<byte[]> message) throws ZMQException;
-}
