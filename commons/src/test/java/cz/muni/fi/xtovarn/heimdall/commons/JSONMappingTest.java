b2820bc7a89611c72139618ee7ffc51ebd773585
Dan Tovarnak <dan.tovarnak@gmail.com>
2012-10-10 14:47:08 +0000

    New test added

--- commons/src/test/java/cz/muni/fi/xtovarn/heimdall/commons/JSONMappingTest.java
+++ commons/src/test/java/cz/muni/fi/xtovarn/heimdall/commons/JSONMappingTest.java
@@ -2,24 +2,98 @@ package cz.muni.fi.xtovarn.heimdall.commons;
 
 import com.fasterxml.jackson.databind.util.ISO8601Utils;
 import cz.muni.fi.xtovarn.heimdall.commons.entity.Event;
+import cz.muni.fi.xtovarn.heimdall.commons.entity.Payload;
 import cz.muni.fi.xtovarn.heimdall.commons.json.JSONStringParser;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import sun.util.resources.TimeZoneNamesBundle;
 
 import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
 
 
 public class JSONMappingTest {
 
-	private final String json = "{\"Event\":{\"occurrenceTime\":\"" + ISO8601Utils.format(new Date(System.currentTimeMillis()), true) + "\",\"hostname\":\"domain.localhost.cz\",\"type\":\"org.linux.cron.Started\",\"application\":\"Cron\",\"process\":\"cron\",\"processId\":\"4219\",\"severity\":5,\"priority\":4,\"Payload\":{\"schema\":\"http://www.linux.org/schema/monitoring/cron/3.1/events.xsd\",\"schemaVersion\":\"3.1\",\"value\":4648,\"value2\":\"Fax4x46aeEF%aax4x%46aeEF\"}}}";
+	long id = 0;
+	Date occurrenceTime = ISO8601Utils.parse("2012-10-12T03:44:52.713+00:00"); // String.replace("+0000", "+00:00") -- bug in ISOUtils, cannot parse +0000
+	Date detectionTime = ISO8601Utils.parse("2012-10-12T03:44:52.791+00:00");
+	String hostname = "domain.localhost.cz";
+	String type = "org.linux.cron.Started";
+	String application = "Cron";
+	String process = "cron";
+	String processId = "4219";
+	int severity = 5;
+	int priority = 4;
+	String schema = "http://cron.org/1.0/events.jsch";
+	String schemaVersion = "1.0";
+	int value1 = 4648;
+	String value2 = "Fax4x46aeEF%aax4x%46aeEF";
+
+	private Event event;
+
+	private final String expectedJson = "{\"Event\":{" +
+			"\"id\":" + id + "," +
+			"\"occurrenceTime\":" + "\"" + ISO8601Utils.format(occurrenceTime, true, TimeZone.getTimeZone("Z")).replace("Z", "+0000") + "\"," +
+			"\"detectionTime\":" + "\"" + ISO8601Utils.format(detectionTime, true,TimeZone.getTimeZone("Z")).replace("Z", "+0000") + "\"," +
+			"\"hostname\":" + "\"" + hostname + "\"," +
+			"\"type\":" + "\"" + type + "\"," +
+			"\"application\":" + "\"" + application + "\"," +
+			"\"process\":" + "\"" + process + "\"," +
+			"\"processId\":" + "\"" + processId + "\"," +
+			"\"severity\":" + severity + "," +
+			"\"priority\":" + priority + "," +
+			"\"Payload\":{" +
+			"\"schema\":" + "\"" + schema + "\"," +
+			"\"schemaVersion\":" + "\"" + schemaVersion + "\"," +
+			"\"value1\":" + value1 +	"," +
+			"\"value2\":"  + "\"" + value2 + "\"" + "}}}";
+
+
+	@Before
+	public void setUp() throws Exception {
+		event = new Event();
+		event.setOccurrenceTime(occurrenceTime);
+		event.setDetectionTime(detectionTime);
+		event.setHostname(hostname);
+		event.setType(type);
+		event.setApplication(application);
+		event.setProcess(process);
+		event.setProcessId(processId);
+		event.setSeverity(severity);
+		event.setPriority(priority);
+
+		Payload payload = new Payload();
+
+		payload.setSchema(schema);
+		payload.setSchemaVersion(schemaVersion);
+		payload.add("value1", value1);
+		payload.add("value2", value2);
+
+		event.setPayload(payload);
+	}
 
 	@Test
-	public void testMapping() throws Exception {
+	public void testMappingEventToJsonString() throws Exception {
 
-		Event event = JSONStringParser.stringToEvent(json);
+		String actualJson = JSONStringParser.eventToString(event);
+		Assert.assertEquals(this.expectedJson, actualJson);
+	}
+
+	@Test
+	public void testMappingJsonToEvent() throws Exception {
+
+		Event actualEvent = JSONStringParser.stringToEvent(expectedJson);
+
+		Assert.assertEquals(this.event, actualEvent);
+
+	}
+
+	@Test
+	public void testMappingEventToJson() throws Exception {
 
 
-		Assert.assertEquals("Fax4x46aeEF%aax4x%46aeEF", event.getPayload().getValue("value2").toString());
 
 	}
 }
