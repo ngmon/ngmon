8a34baecd57c634e04f7671328cc0bb08c0840ad
Svata Novak <324881@mail.muni.cz>
2013-03-07 16:35:12 +0100

    Made the whole addSubscription() method synchronized (probably wasn't thread-safe before)

--- pubsub/src/main/java/cz/muni/fi/xtovarn/heimdall/pubsub/SubscriptionManager.java
+++ pubsub/src/main/java/cz/muni/fi/xtovarn/heimdall/pubsub/SubscriptionManager.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 import cz.muni.fi.publishsubscribe.countingtree.CountingTree;
 import cz.muni.fi.publishsubscribe.countingtree.Predicate;
@@ -13,15 +12,11 @@ import cz.muni.fi.publishsubscribe.countingtree.Subscription;
 public class SubscriptionManager {
 
 	private CountingTree countingTree = new CountingTree();
-	private Map<Integer, Collection<Subscription>> connectionIdToSubscriptions = new ConcurrentHashMap<>();
-	
-	private synchronized void subscribe(Predicate predicate, Subscription subscription) {
-		this.countingTree.subscribe(predicate, subscription);
-	}
+	private Map<Integer, Collection<Subscription>> connectionIdToSubscriptions = new HashMap<>();
 
-	public Subscription addSubscription(Integer connectionId, Predicate predicate) {
+	public synchronized Subscription addSubscription(Integer connectionId, Predicate predicate) {
 		Subscription subscription = new Subscription();
-		this.subscribe(predicate, subscription);
+		this.countingTree.subscribe(predicate, subscription);
 
 		Collection<Subscription> subscriptions = connectionIdToSubscriptions.get(connectionId);
 		if (subscriptions == null) {
