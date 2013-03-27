0d7a2030376597a9c073b15b677e702da9093a66
Svata Novak <324881@mail.muni.cz>
2013-03-27 11:32:36 +0100

    Wrapped exceptions

--- client/src/main/java/cz/muni/fi/xtovarn/heimdall/client/ClientConnectionFactory.java
+++ client/src/main/java/cz/muni/fi/xtovarn/heimdall/client/ClientConnectionFactory.java
@@ -6,14 +6,45 @@ import java.util.concurrent.TimeoutException;
 
 public class ClientConnectionFactory {
 
+	public static class ConnectionException extends Exception {
+
+		public ConnectionException() {
+			super();
+		}
+
+		public ConnectionException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+			super(arg0, arg1, arg2, arg3);
+		}
+
+		public ConnectionException(String arg0, Throwable arg1) {
+			super(arg0, arg1);
+		}
+
+		public ConnectionException(String arg0) {
+			super(arg0);
+		}
+
+		public ConnectionException(Throwable arg0) {
+			super(arg0);
+		}
+
+	}
+
 	public static ClientApi getClient(String login, String passcode, long timeout, TimeUnit unit)
-			throws InterruptedException, ExecutionException, TimeoutException {
-		Client client = new Client(timeout, unit);
-		// wait for the channel to connect (waiting in the constructor is not enough)
-		client.getChannelConnectedResult().get();
+			throws ConnectionException {
+		Client client;
 		// wait for the server to send CONNECTED (or ERROR)
-		boolean connected = client.connect(login, passcode).get(timeout, unit);
-		
+		boolean connected;
+		try {
+			client = new Client(timeout, unit);
+			// wait for the channel to connect (waiting in the constructor is not
+			// enough)
+			client.getChannelConnectedResult().get();
+			connected = client.connect(login, passcode).get(timeout, unit);
+		} catch (InterruptedException | ExecutionException | TimeoutException e) {
+			throw new ConnectionException(e);
+		}
+
 		return connected ? client : null;
 	}
 
