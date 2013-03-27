1bcaa8b409fd9bc1110d5cfbc5cd4185a7dffa14
Svata Novak <324881@mail.muni.cz>
2013-03-27 15:11:24 +0100

    Small changes to state machine - added getEndStates(), fixed variable name (transition -> transitions)

--- fsm/src/main/java/cz/muni/fi/xtovarn/fsm/AbstractFiniteStateMachineNoActions.java
+++ fsm/src/main/java/cz/muni/fi/xtovarn/fsm/AbstractFiniteStateMachineNoActions.java
@@ -3,84 +3,86 @@ package cz.muni.fi.xtovarn.fsm;
 import java.util.*;
 
 public abstract class AbstractFiniteStateMachineNoActions<T1 extends Enum<T1>, T2> {
-    private final Class<T1> statesEnumClass; // Generics Workaround
+	private final Class<T1> statesEnumClass; // Generics Workaround
 
-    private Map<T1, Map<T2, T1>> stateTransitionFunction;
+	private Map<T1, Map<T2, T1>> stateTransitionFunction;
 
-    private final T1 startState;
-    private final Set<T1> endStates;
-    private T1 currentState;
+	private final T1 startState;
+	private final Set<T1> endStates;
+	private T1 currentState;
 
 	private final boolean DEBUG;
 
-    public AbstractFiniteStateMachineNoActions(T1 startState, T1[] endStates, Class<T1> statesEnumClass, boolean debug) {
-        this.statesEnumClass = statesEnumClass;
+	public AbstractFiniteStateMachineNoActions(T1 startState, T1[] endStates, Class<T1> statesEnumClass, boolean debug) {
+		this.statesEnumClass = statesEnumClass;
 
-        this.stateTransitionFunction = new EnumMap<>(statesEnumClass);
-        this.startState = startState;
-        this.endStates = new HashSet<>(Arrays.asList(endStates));
-        this.currentState = startState;
-	    this.DEBUG = debug;
-    }
-    
-    public AbstractFiniteStateMachineNoActions(T1 startState, T1[] endStates, Class<T1> statesEnumClass) {
-    	this(startState, endStates, statesEnumClass, false);
-    }
+		this.stateTransitionFunction = new EnumMap<>(statesEnumClass);
+		this.startState = startState;
+		this.endStates = new HashSet<>(Arrays.asList(endStates));
+		this.currentState = startState;
+		this.DEBUG = debug;
+	}
 
-    public void addTransition(T1 sourceState, T2 symbol, T1 targetState) {
-        if (endStates.contains(sourceState)) {
-            throw new IllegalArgumentException("You cannot create transition from an end state");
-        }
+	public AbstractFiniteStateMachineNoActions(T1 startState, T1[] endStates, Class<T1> statesEnumClass) {
+		this(startState, endStates, statesEnumClass, false);
+	}
 
-        Map<T2, T1> transition = stateTransitionFunction.get(sourceState);
+	public void addTransition(T1 sourceState, T2 symbol, T1 targetState) {
+		if (endStates.contains(sourceState)) {
+			throw new IllegalArgumentException("You cannot create transition from an end state");
+		}
 
-        if (transition == null) {
-            transition = new HashMap<>();
-            transition.put(symbol, targetState);
-            stateTransitionFunction.put(sourceState, transition);
-        } else {
-            transition.put(symbol, targetState);
-        }
+		Map<T2, T1> transitions = stateTransitionFunction.get(sourceState);
 
-    }
+		if (transitions == null) {
+			transitions = new HashMap<>();
+			transitions.put(symbol, targetState);
+			stateTransitionFunction.put(sourceState, transitions);
+		} else {
+			transitions.put(symbol, targetState);
+		}
 
-    // is transition allowed?
-    public synchronized boolean isAllowed(T2 symbol) {
-        return getNextState(symbol) != null;
+	}
 
-    }
+	// is transition allowed?
+	public synchronized boolean isAllowed(T2 symbol) {
+		return getNextState(symbol) != null;
 
-    public synchronized void readSymbol(T2 symbol) {
-        T1 nextState = getNextState(symbol);
+	}
 
-        if (nextState == null) {
-            throw new IllegalArgumentException(String.format(
-                    "No such transition under given symbol(%s) from state %s!",
-                    symbol.toString(),
-                    this.currentState));
-        }
+	public synchronized void readSymbol(T2 symbol) {
+		T1 nextState = getNextState(symbol);
 
-	    if (DEBUG) {
-		    System.out.println(String.format("[%s] %s: %s -> %s", this.toString(), symbol, currentState, nextState));
-	    }
+		if (nextState == null) {
+			throw new IllegalArgumentException(String.format(
+					"No such transition under given symbol(%s) from state %s!", symbol.toString(), this.currentState));
+		}
 
-        this.currentState = nextState;
-    }
+		if (DEBUG) {
+			System.out.println(String.format("[%s] %s: %s -> %s", this.toString(), symbol, currentState, nextState));
+		}
 
-    public synchronized T1 getNextState(T2 symbol) {
-        return this.stateTransitionFunction.get(this.currentState).get(symbol);
-    }
+		this.currentState = nextState;
+	}
 
-    public synchronized boolean isEnded() {
-        return this.endStates.contains(this.currentState);
-    }
+	public synchronized T1 getNextState(T2 symbol) {
+		return this.stateTransitionFunction.get(this.currentState).get(symbol);
+	}
 
-    public synchronized T1 getCurrentState() {
-        return this.currentState;
-    }
+	public synchronized boolean isEnded() {
+		return this.endStates.contains(this.currentState);
+	}
 
-    public synchronized T1 getStartState() {
-        return this.startState;
-    }
+	public synchronized T1 getCurrentState() {
+		return this.currentState;
+	}
+
+	public synchronized T1 getStartState() {
+		return this.startState;
+	}
+
+	public synchronized Set<T1> getEndStates() {
+		return endStates;
+	}
 
 }
