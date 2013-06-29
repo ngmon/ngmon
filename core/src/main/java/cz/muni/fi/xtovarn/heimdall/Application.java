6bda36f832e600a3558de27a9a616193a72c5128
xtovarn <dan.tovarnak@gmail.com>
2013-06-29 09:09:48 +0200

    * refactor

--- core/src/main/java/cz/muni/fi/xtovarn/heimdall/Application.java
+++ core/src/main/java/cz/muni/fi/xtovarn/heimdall/Application.java
@@ -1,38 +1,25 @@
 package cz.muni.fi.xtovarn.heimdall;
 
 import com.sleepycat.je.DatabaseException;
-
-import java.io.File;
-import java.io.IOException;
-
+import cz.muni.fi.xtovarn.heimdall.util.NgmonLauncher;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.io.File;
+import java.io.IOException;
 /**
  * Used to start the server directly (from the command line)
  */
 public class Application {
 
-	public static final String DATABASE_PATH = "./database/events";
-	public static NgmonServer ngmonServer;
+	public static final File DATABASE_PATH = new File("db");
+	public static NgmonLauncher ngmonLauncher;
 	
 	private static Logger logger = LogManager.getLogger(Application.class);
 
 	public static void main(String[] args) throws IOException, DatabaseException, InterruptedException {
+        ngmonLauncher = new NgmonLauncher(DATABASE_PATH);
 
-		Runtime.getRuntime().addShutdownHook(new Thread(new ShutdownHandler()));
-		ngmonServer = new NgmonServer(new File(DATABASE_PATH));
-
-		logger.info("Heimdall is starting...");
-		ngmonServer.start();
+        ngmonLauncher.start();
 	}
-
-	static class ShutdownHandler implements Runnable {
-		@Override
-		public void run() {
-			logger.info("Shutting down...");
-			ngmonServer.stop();
-		}
-	}
-
 }
