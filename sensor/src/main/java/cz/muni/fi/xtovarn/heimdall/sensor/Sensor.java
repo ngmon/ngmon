2299cba02348b0531e63597b1125169b671a8b90
xtovarn <dan.tovarnak@gmail.com>
2013-06-29 09:17:46 +0200

    * attributes changed

--- sensor/src/main/java/cz/muni/fi/xtovarn/heimdall/sensor/Sensor.java
+++ sensor/src/main/java/cz/muni/fi/xtovarn/heimdall/sensor/Sensor.java
@@ -20,7 +20,7 @@ public class Sensor {
 		while (i > 0) {
 			Thread.sleep(1);
 			i--;
-			String json = "{\"Event\":{\"occurrenceTime\":\"" + ISO8601Utils.format(new Date(System.currentTimeMillis()), true) + "\",\"hostname\":\"domain.localhost.cz\",\"type\":\"org.linux.cron.Started\",\"application\":\"Cron\",\"process\":\"cron\",\"processId\":\"4219\",\"severity\":5,\"priority\":4,\"Payload\":{\"schema\":\"http://www.linux.org/schema/monitoring/cron/3.1/events.xsd\",\"schemaVersion\":\"3.1\",\"value\":4648,\"value2\":\"Fax4x46aeEF%aax4x%46aeEF\"}}}";
+			String json = "{\"Event\":{\"occurrenceTime\":\"" + ISO8601Utils.format(new Date(System.currentTimeMillis()), true) + "\",\"hostname\":\"domain.localhost.cz\",\"type\":\"org.linux.cron.Started\",\"application\":\"Cron\",\"process\":\"cron\",\"processId\":\"4219\",\"level\":2,\"_\":{\"schema\":\"http://www.linux.org/schema/monitoring/cron/3.1/events.xsd\",\"schemaVersion\":\"3.1\",\"value\":4648,\"value2\":\"Fax4x46aeEF%aax4x%46aeEF\"}}}";
 			out.write(json);
 			out.newLine();
 			out.flush();
